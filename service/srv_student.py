d72093343b115859930ecb23897b7e9e33f1b76b
VGDANIEL <www.vargadaniel1999@gmail.com>
2025-11-15 17:25:15 +0200

    Commit

--- service/srv_student.py
+++ service/srv_student.py
@@ -4,3 +4,5 @@ class service_student:
     def __init__(self, repo_studenti, validator_studenti):
         self.__repo = repo_studenti
         self.__validator = validator_studenti
+
+    def adauga_studenti(self, id_sti):
